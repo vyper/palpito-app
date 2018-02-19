142076a31a0044a144e65f92300ac83b0f5e9511
Leonardo Saraiva <vyper@maneh.org>
2018-02-18 22:52:58 -0300

    Showing more information about game on Bets screen

--- Bets.js
+++ Bets.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { Alert, FlatList, Text, TouchableHighlight, View } from 'react-native';
 
 export default class Bets extends Component {
   state = {
@@ -8,11 +8,12 @@ export default class Bets extends Component {
 
   async componentWillMount() {
     const bets = await this.requestBets();
+    console.log(bets);
     this.setState({ bets });
   }
 
   requestBets() {
-    return fetch('http://palpito.com.br/bets.json?day=2016-07-03+00%3A00%3A00+UTC', {
+    return fetch('http://palpito.com.br/bets.json?group_id=13', {
       headers: { Authorization: 'Bearer ' + this.props.accessToken }
     }).then((response) => response.json())
     .catch((error) => {
@@ -23,7 +24,15 @@ export default class Bets extends Component {
   _keyExtractor = (item, index) => item.id;
 
   _renderItem = ({item}) => (
-    <Text>{item.team_home}</Text>
+    <TouchableHighlight onPress={() => { Alert.alert(`open ${item.id}`) }}>
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+        <Text style={{ fontSize: 20, paddingTop: 15, paddingBottom: 15, flex: 5, textAlign: 'right' }}>{item.team_home}</Text>
+        <Text style={{ fontSize: 20, paddingTop: 15, paddingBottom: 15, flex: 1, textAlign: 'right' }}>{item.team_home_goals}</Text>
+        <Text style={{ fontSize: 10, paddingTop: 15, paddingBottom: 15, flex: 1, textAlign: 'center' }}>x</Text>
+        <Text style={{ fontSize: 20, paddingTop: 15, paddingBottom: 15, flex: 1, textAlign: 'left' }}>{item.team_away_goals}</Text>
+        <Text style={{ fontSize: 20, paddingTop: 15, paddingBottom: 15, flex: 5, textAlign: 'left' }}>{item.team_away}</Text>
+      </View>
+    </TouchableHighlight>
   );
 
   render() {
