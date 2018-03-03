7e95c0e1dddf8b0d5c7e907956b4b9e67661adfe
Leonardo Saraiva <vyper@maneh.org>
2018-03-03 15:46:45 -0300

    Add & configure react-native-config

--- android/app/src/main/java/com/palpito/MainApplication.java
+++ android/app/src/main/java/com/palpito/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.smixx.fabric.FabricPackage;
 import com.facebook.react.ReactNativeHost;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
           new FBSDKPackage(mCallbackManager),
           new FabricPackage()
       );
